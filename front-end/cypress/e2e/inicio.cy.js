3b03614e8af67cc2611580b4a97bb027e2388572
unknown <JhuanSilva>
2024-08-16 15:07:45 -0300

    :arrow_up:atualizacao do codigo arrumado

--- front-end/cypress/e2e/inicio.cy.js
+++ front-end/cypress/e2e/inicio.cy.js
@@ -3,13 +3,15 @@ describe('Página Principal', () => {
     beforeEach( () => {
 			cy.visit('http://localhost:3000/')
 		})
-    cy.get('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   })
 
   it('Deve renderizar textos de vantagens', () => {
-    cy.get('semTarifa').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
-    cy.get('sacarGratuitamente').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
-    cy.get('acumularPontos').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
-    cy.get('dispositivosProtegidos').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
+    cy.getByData('semTarifa').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+    cy.getByData('sacarGratuitamente').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
+    cy.getByData('acumularPontos').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
+    cy.getByData('dispositivosProtegidos').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
   })
+
+  it('')
 })
