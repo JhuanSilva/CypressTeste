7e228888df412300a812b5c4dd9b8878a23c0618
unknown <JhuanSilva>
2024-08-16 09:37:29 -0300

    :bolb:adicionando funcao a porta

--- front-end/cypress/e2e/inicio.cy.js
+++ front-end/cypress/e2e/inicio.cy.js
@@ -1,6 +1,8 @@
 describe('Página Principal', () => {
   it('Deve renderizar h1 com o texto correto!', () => {
-    cy.visit('http://localhost:3000')
+    beforeEach( () => {
+			cy.visit('http://localhost:3000/')
+		})
     cy.get('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   })
 })
