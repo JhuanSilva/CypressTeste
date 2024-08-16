eb4fab85c68215dffa8fd7b350c1c265691d4b64
unknown <JhuanSilva>
2024-08-16 11:08:38 -0300

    :test:criacao comando para verificar

--- front-end/cypress/e2e/inicio.cy.js
+++ front-end/cypress/e2e/inicio.cy.js
@@ -1,8 +1,10 @@
 describe('Página Principal', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
   it('Deve renderizar h1 com o texto correto!', () => {
-    beforeEach( () => {
-			cy.visit('http://localhost:3000/')
-		})
-    cy.get('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.ContainText('titulo-principal', 'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   })
 })
