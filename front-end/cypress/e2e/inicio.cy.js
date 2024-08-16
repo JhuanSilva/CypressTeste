65a1a4c07aa767bb461ac0de4f86aa53c5bb99d2
unknown <JhuanSilva>
2024-08-16 14:59:22 -0300

    :feat: renderizacao de texto vantagens

--- front-end/cypress/e2e/inicio.cy.js
+++ front-end/cypress/e2e/inicio.cy.js
@@ -5,4 +5,11 @@ describe('Página Principal', () => {
 		})
     cy.get('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   })
+
+  it('Deve renderizar textos de vantagens', () => {
+    cy.get('semTarifa').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+    cy.get('sacarGratuitamente').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
+    cy.get('acumularPontos').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
+    cy.get('dispositivosProtegidos').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
+  })
 })
