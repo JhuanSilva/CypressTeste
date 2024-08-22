672c79e8a9060c419b77de77fc356faaebfc2d24
unknown <JhuanSilva>
2024-08-22 15:07:13 -0300

    :sparkles::feat: Inserindo formulario de cadastro

--- front-end/cypress/e2e/formularioLogin.cy.js
+++ front-end/cypress/e2e/formularioLogin.cy.js
@@ -10,7 +10,7 @@ describe('Formulario de login', () => {
     cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
   })
 
-  it('Não deve permitir um campo em branco!', () => {
+  it.only('Não deve permitir um campo em branco!', () => {
     cy.getByData('botao-login').click()
     //cy.getByData('email-input).type("")
     cy.getByData('senha-input').type('123456')
