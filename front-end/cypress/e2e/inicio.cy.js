53eb5bce3624ca704f408d939342c8c62105b500
unknown <JhuanSilva>
2024-08-22 11:37:22 -0300

    :test_tube: Inserir elementos da pagina

--- front-end/cypress/e2e/inicio.cy.js
+++ front-end/cypress/e2e/inicio.cy.js
@@ -3,20 +3,13 @@ describe('Página Principal', () => {
     beforeEach( () => {
 			cy.visit('http://localhost:3000/')
 		})
-    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    it('Deve renderizar o titulo principal com o texto correto', () => {
+      cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+      })
+    })
+
+  it('Deve carregar os h2', () => {
+    cy.getByData('h2').contains('Vantagens do nosso banco:')
   })
 
-  it('Deve renderizar textos de vantagens', () => {
-    cy.getByData('semTarifa').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
-    cy.getByData('sacarGratuitamente').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
-    cy.getByData('acumularPontos').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
-    cy.getByData('dispositivosProtegidos').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
-  })
-
-  it('Deve renderizar os h3', () => {
-    cy.getByData('h3ContaGratuita').contains('Conta e cartão gratuitos')
-    cy.getByData('h3SacarGratuitamente').contains('Saques sem custo')
-    cy.getByData('h3AcumularPontos').contains('Programa de pontos')
-    cy.getByData('h3SeguroDispositivos').contains('Seguro Dispositivos')
-  })
 })
--- front-end/src/paginas/Inicio/index.jsx
+++ front-end/src/paginas/Inicio/index.jsx
@@ -17,15 +17,18 @@ export default function Inicio() {
               Crie sua conta com a gente!
             </h1>
             <img
+              data-test="imagem01"
               src={ilustracaoBanner}
               alt="Gráficos e uma pessoa com dinheiro na mão"
             />
           </div>
           <div className={estilos.vantagens}>
-            <h2>Vantagens do nosso banco:</h2>
+            <h2 data-test="h2">
+              Vantagens do nosso banco:
+              </h2>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
-                <img src={presente} alt="ícone de um presente" />
+                <img data-test="imagem02" src={presente} alt="ícone de um presente" />
                 <h3 data-test="h3ContaGratuita">Conta e cartão gratuitos</h3>
                 <p data-test="semTarifa">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
@@ -33,7 +36,7 @@ export default function Inicio() {
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
+                <img data-test="imagem03" src={saque} alt="ícone de uma mão segurando um dinheiro" />
                 <h3 data-test="h3SacarGratuitamente">Saques sem custo</h3>
                 <p data-test="sacarGratuitamente">
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
@@ -41,7 +44,7 @@ export default function Inicio() {
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img src={estrela} alt="ícone de uma estrela" />
+                <img data-test="imagem04" src={estrela} alt="ícone de uma estrela" />
                 <h3 data-test="h3AcumularPontos">Programa de pontos</h3>
                 <p data-test="acumularPontos">
                   Você pode acumular pontos com suas compras no crédito sem
@@ -50,6 +53,7 @@ export default function Inicio() {
               </div>
               <div className={estilos.vantagens__item}>
                 <img
+                  data-test="imagem05"
                   src={dispositivo}
                   alt="ícone de um dispositivo como notebook"
                 />
