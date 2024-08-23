2dbe2b408dd1c75632ec3f754e9438abbef4d890
unknown <JhuanSilva>
2024-08-23 10:59:42 -0300

    :sparkles::feat: Validando pagina de cartao

---
+++ front-end/cypress/e2e/paginas.cy.js
@@ -0,0 +1,9 @@
+describe('Testando multiplas paginas', () => {
+  it('Deve conseguir acessar a páginas de cartões', () => {
+    cy.visit('/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+  })
+})
