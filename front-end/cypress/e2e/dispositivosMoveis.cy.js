9816ecf51546eccb49f8e0b4804747a1608def09
unknown <JhuanSilva>
2024-08-29 13:45:35 -0300

    :test_tube:Criando diferentes displays

--- front-end/cypress/e2e/dispositivosMoveis.cy.js
+++ front-end/cypress/e2e/dispositivosMoveis.cy.js
@@ -1,6 +1,34 @@
 describe('Testando dispositivos móveis', () => {
+  context('Resolução de 720p', () => {
+    beforeEach(() => {
+      cy.viewport(1280, 720)
+    })
+  })
+
+  context('Resolução de macbook-11', () => {
+    beforeEach(() => {
+      cy.viewport(1366, 756)
+      cy.viewport('macbook-11')
+    })
+  })
+
+  context('Resolução de iphone-5', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-5')
+    })
+  })
+
+  context('Resolução de ipad-mini', () => {
+    beforeEach(() => {
+      cy.viewport(2266, 1488)
+      cy.viewport('ipad-mini')
+    })
+  })
+
   it('Deve existir um menu burguer', () => {
     cy.visit('/')
+
+    cy.viewport('ipad-mini', 'landscape')
     
     cy.getByData('botao-login').click()
     cy.getByData('email-input').type('neilton@alura.com')
